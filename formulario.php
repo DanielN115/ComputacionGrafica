9d0b5eecb1f4b5ede7051ce275098be65f05c2fc
Brayan Piandoy <sebas310393@gmail.com>
2023-02-03 13:55:42 -0500

    cambio2

--- formulario.php
+++ formulario.php
@@ -1,7 +1,23 @@
 <?php
+
+$host='localhost';
+$bd='Pag_ventas';
+$user='postgres';
+$pass='juanchoydani';
+
+$conexion=pg_connect("host=$host dbname=$bd user=$user password=$pass");
+
+$query=("INSERT INTO clientes(idcliente,sname,genero,fechnacimiento,direccion,barrio,telefono,correo,contraseña) 
+VALUES('$_REQUEST[identificacion]','$_REQUEST[nombre]','$_REQUEST[genero]','$_REQUEST[fechanac]',
+'$_REQUEST[direccion]','$_REQUEST[barrio]','$_REQUEST[telefono]','$_REQUEST[vercorreo]',
+'$_REQUEST[vercontraseña]')");
+
+$consulta=pg_query($conexion,$query);
+pg_close();
+
     echo "<br>";
-    echo "su nombre es";
-    echo $_POST[nombre]
+    echo "cliente resgistrado";
+
 
 
 ?>
