011d6425be12cfd20f93e3c29000a67481490775
Brayan Piandoy <sebas310393@gmail.com>
2023-02-09 18:46:09 -0500

    cambio5- implementacion de verificacion de usuario

---
+++ sesion.php
@@ -0,0 +1,18 @@
+<?php
+
+require 'conexion_postgres.php';
+session_start();
+
+$usuario=$_POST['usuario'];
+$contraseña=$_POST['contraseña'];
+
+$query="SELECT * FROM clientes WHERE correo = '$usuario' AND contraseña = '$contraseña'";
+$consulta=pg_query($conexion,$query);
+$cantidad=pg_num_rows($consulta);
+if($cantidad>0){
+    $_SESSION['nombre_usuario']=$usuario;
+    header("location: ingreso.php");   
+}else{
+    echo "Datos incorrectos";
+}
+?>
