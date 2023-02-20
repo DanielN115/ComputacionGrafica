510312adc57ff66178c9fec6082811e27925bec0
DanielN115 <124371909+DanielN115@users.noreply.github.com>
2023-02-19 20:13:58 -0500

    Create pago.php

---
+++ pago.php
@@ -0,0 +1,20 @@
+<?php
+
+$host='localhost';
+$bd='Pag_ventas';
+$user='postgres';
+$pass='juanchoydani';
+
+$conexion=pg_connect("host=$host dbname=$bd user=$user password=$pass");
+
+$query=("INSERT INTO table() VALUES() );
+
+$consulta=pg_query($conexion,$query);
+pg_close();
+
+    echo "<br>";
+    echo "PAGO HECHO";
+
+
+
+?>
