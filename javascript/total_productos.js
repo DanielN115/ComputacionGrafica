7e3b87b91c4238d6ac84053c7c9068f6bc771e66
andreschacua <andresfernando990115@gmail.com>
2023-02-20 23:14:03 -0500

    Progreso Mostrar Total

--- javascript/total_productos.js
+++ javascript/total_productos.js
@@ -1,24 +1,19 @@
+var total_compra = 0;
 function multiplicar1() {
     // Obtiene la cantidad ingresada por el usuario
     var cantidad = document.getElementById("quantity1").value;
     var precio = document.getElementById("precio1").getAttribute("data-precio");
     var total = cantidad * precio;
+    total_compra += total;
     document.getElementById("resultado1").innerHTML = total;
 
-    /*  
-    document.getElementById("presiototal").innerHTML = document.getElementById("resultado1").value +
-                                                        document.getElementById("resultado2").value +
-                                                        document.getElementById("resultado3").value +
-                                                        document.getElementById("resultado4").value +
-                                                        document.getElementById("resultado5").value +
-                                                        document.getElementById("resultado6").value;
-    */
     }
     function multiplicar2() {
     // Obtiene la cantidad ingresada por el usuario
     var cantidad = document.getElementById("quantity2").value;
     var precio = document.getElementById("precio2").getAttribute("data-precio");
     var total = cantidad * precio;
+    total_compra += total;
     document.getElementById("resultado2").innerHTML = total;
     }
     function multiplicar3() {
@@ -26,6 +21,7 @@ function multiplicar1() {
     var cantidad = document.getElementById("quantity3").value;
     var precio = document.getElementById("precio3").getAttribute("data-precio");
     var total = cantidad * precio;
+    total_compra += total;
     document.getElementById("resultado3").innerHTML = total;
     }
     function multiplicar4() {
@@ -33,6 +29,7 @@ function multiplicar1() {
     var cantidad = document.getElementById("quantity4").value;
     var precio = document.getElementById("precio4").getAttribute("data-precio");
     var total = cantidad * precio;
+    total_compra += total;
     document.getElementById("resultado4").innerHTML = total;
     }
     function multiplicar5() {
@@ -40,6 +37,7 @@ function multiplicar1() {
     var cantidad = document.getElementById("quantity5").value;
     var precio = document.getElementById("precio5").getAttribute("data-precio");
     var total = cantidad * precio;
+    total_compra += total;
     document.getElementById("resultado5").innerHTML = total;
     }
     function multiplicar6() {
@@ -47,5 +45,11 @@ function multiplicar1() {
     var cantidad = document.getElementById("quantity6").value;
     var precio = document.getElementById("precio6").getAttribute("data-precio");
     var total = cantidad * precio;
+    total_compra += total;
     document.getElementById("resultado6").innerHTML = total;
     }
+
+    function dar_total() {
+        document.getElementById("total_compra").innerHTML = total_compra;
+    }
+
